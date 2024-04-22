3ad0214f938dff58ea5094f55d7ae08cfead033b
PhamVanThanh <phamtrung21112003@gmail.com>
2024-04-22 19:15:20 +0700

    fix resize img HoSo_GUI

--- src/main/java/gui/HoSo_GUI.java
+++ src/main/java/gui/HoSo_GUI.java
@@ -240,7 +240,7 @@ public class HoSo_GUI extends JPanel {
 	
 	private ImageIcon resizeImage(ImageIcon imageIcon) {
 		Image image = imageIcon.getImage();
-		Image newImage = image.getScaledInstance(64, 64, Image.SCALE_SMOOTH);
+		Image newImage = image.getScaledInstance(256, 256, Image.SCALE_SMOOTH);
 		return new ImageIcon(newImage);
 	}
 }
